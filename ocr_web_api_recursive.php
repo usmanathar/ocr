3b50f6df22d0dee0156a377a29c24206f41070e3
usmanathar <usman.athar@gmail.com>
2023-07-28 13:22:22 +0500

    updated3 commit 28july

--- ocr_web_api_recursive.php
+++ ocr_web_api_recursive.php
@@ -1,17 +1,4 @@
 <?php
-include 'SFTP/vendor/autoload.php';
- 
-$sftp = new \phpseclib\Net\SFTP('23.122.104.252');
-if (!$sftp->login('ocr_lrwic', 'the_proxy@OCR')) {
-	exit('Login Failed');
-}
-
-echo $sftp->pwd() . "\r\n";
-$sftp->put('filename.ext', 'hello, world!');
-print_r($sftp->nlist());
-
-exit("==-==");
-
 set_time_limit(0); 
 //require_once('C:/ocr/SFTP/vendor/autoload.php');
 require_once('/var/www/html/ocr/SFTP/vendor/autoload.php');
