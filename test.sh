db0ad5505944602248f09fd0134c23d2defa2552
usmanathar <usman.athar@gmail.com>
2023-07-30 14:11:06 +0500

    shell_script commit 30july

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#check for root
+UID=$(id -u)
+if [ x$UID != x0 ] 
+then
+    #Beware of how you compose the command
+    printf -v cmd_str '%q ' "$0" "$@"
+    exec sudo su -c "$cmd_str"
+fi
+
+#I am root
+mkdir /opt/D3GO/
+#and the rest of your commands
