ba84c6203b992f18aef82930b46481bafecfc438
usmanathar <usman.athar@gmail.com>
2023-07-30 14:42:19 +0500

    shell_script1.2 commit 30july

--- test.sh
+++ test.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-echo "In test script"
-#check for root
-UID=$(id -u)
-if [ x$UID != x0 ] 
-then
-    #Beware of how you compose the command
-    printf -v cmd_str '%q ' "$0" "$@"
-    exec sudo su -c "$cmd_str"
-fi
-
-#I am root
-mkdir /opt/D3GO/
-#and the rest of your commands
+ls
