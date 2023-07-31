d0844562f54f4625b2086bd731c7c06fcf3e6921
usmanathar <usman.athar@gmail.com>
2023-07-31 19:55:47 +0500

    sftp commit 31july

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
-     //include 'SFTP/vendor/autoload.php';
+     include 'SFTP/vendor/autoload.php';
  
-     /*$sftp = new \phpseclib\phpseclib\Net\SFTP('23.122.104.252');
+     $sftp = new \phpseclib\phpseclib\Net\SFTP('23.122.104.252');
      if (!$sftp->login('ocr_lrwic', 'the_proxy@OCR')) {
          exit('Login Failed');
      }
 
      echo $sftp->pwd() . "\r\n";
      $sftp->put('filename.ext', 'hello, world!');
-     print_r($sftp->nlist());*/
+     print_r($sftp->nlist());
   ?>
   <?php
-     include 'vendor/autoload.php';
+     /*include 'vendor/autoload.php';
  
      $sftp = new \phpseclib3\Net\SFTP('23.122.104.252');
      if (!$sftp->login('ocr_lrwic', 'the_proxy@OCR')) {
@@ -20,5 +20,5 @@
  
      echo $sftp->pwd() . "\r\n";
      $sftp->put('filename.ext', 'hello, world!');
-     print_r($sftp->nlist());
+     print_r($sftp->nlist());*/
   ?>
