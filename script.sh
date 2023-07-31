49913aa76e111edffde861f3599bf71932b718d7
usmanathar <usman.athar@gmail.com>
2023-07-31 19:09:31 +0500

    sftp commit 31july

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sftp ocr_lrwic@OCR
+sshpass -p the_proxy@OCR sftp -oBatchMode=no -b - sftp ocr_lrwic@OCR23.122.104.252 << !
+   cd /var/www/html/public_html/faxFiles/facility_21
+   ls -l
+   #get *.pdf
+   bye
+!
