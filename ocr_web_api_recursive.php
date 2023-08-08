d0b4ac1f4ed4ae1a9782ab536e254fa277ba1fb3
usmanathar <usman.athar@gmail.com>
2023-08-08 18:44:29 +0500

    SFTP  8thAug

--- ocr_web_api_recursive.php
+++ ocr_web_api_recursive.php
@@ -27,8 +27,8 @@ else{
 $pathname ='/var/www/html/public_html/faxFiles/facility_21';
 
 $files = $sftp->nlist($pathname);
-//print_r($files);
-//exit("=-098");
+print_r($files);
+exit("=-098");
 foreach ($files as $file) {
 			  
 	if (substr($file, 0, 1) == '.') continue;
